473081b67ba870a9f427a56cba45cec515b9b31b
Bastien <bastien.garnier@live.fr>
2021-10-08 17:27:44 +0200

    dao db sqlite

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import sqlite3
-
-
+import hashlib
+import populate_sqlite3 as db
 
 app = Flask(__name__, template_folder='templates')
 
@@ -26,6 +26,15 @@ def URL_login():
 def URL_register():
     if request.method == 'POST':
         username = request.form['username']
+        password = request.form['password']
+        key = hashlib.pbkdf2_hmac( #mots de pass hash
+            'sha256', # The hash digest algorithm for HMAC
+            password.encode('utf-8'), # Convert the password to bytes
+            salt, # Provide the salt
+            100000 # It is recommended to use at least 100,000 iterations of SHA-256 
+        )
+        db.auth_user(username,password)
+        
    
     resp = make_response(render_template('home_user.html'))
     resp.set_cookie('userID', user)
@@ -35,3 +44,6 @@ def URL_register():
 @app.route('/home', methods=['GET'])
 def URL_home():
     return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run()
--- populate_sqlite3.py
+++ populate_sqlite3.py
@@ -8,10 +8,37 @@ with open("database", "w") as fp:
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
-
 sql_file = open("sqlite3_init.sql")
 sql_as_string = sql_file.read()
 cursor.executescript(sql_as_string)
+connection.commit()
 
-for row in cursor.execute("SELECT * FROM user"):
-    print(row)
+def username_exist(username): #return bool
+    for row in cursor.execute("SELECT username FROM user"):
+        if(username == row):
+            return True
+    return False
+
+def auth_user(username,passhash): #return bool
+    for row in cursor.execute("SELECT username,hash FROM user"):
+        print(row[0],row[1])
+        if(username == row[0] and passhash==row[1] ):
+            return True
+    return False
+
+def create_user(username,passhash):
+    cursor.execute("SELECT max(user_id) FROM user")
+    print(cursor)
+    for row in cursor.fetchall():
+        print(type(row))
+        print("yo : "+str(row))
+    result = cursor.fetchmany(2)
+    print(result)
+    #cursor.execute("INSERT INTO user(user_id, username, hash) VALUES (?,?,?)",(cursor["user_id"],username,passhash))
+
+def all_user():
+    for row in cursor.execute("SELECT * FROM user"):
+        print(row)
+
+create_user("mama","mdp")
+#all_user()
