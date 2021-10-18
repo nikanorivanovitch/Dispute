a223bacdf6d14bb6e578929f595a17f68b154e78
nikanorivanovitch <andre.jacques64@gmail.com>
2021-10-18 15:15:59 +0200

    Ébauche de base SQL

--- sqlite3_init.sql
+++ sqlite3_init.sql
@@ -1,1 +1,4 @@
-CREATE TABLE user(user_id INT PRIMARY KEY, username VARCHAR(30), hash VARCHAR(64));
+CREATE TABLE IF NOT EXISTS user(user_id INT PRIMARY KEY NOT NULL, user_name VARCHAR(30), user_hash VARCHAR(64), user_email VARCHAR(100));
+CREATE TABLE IF NOT EXISTS server(server_id INT PRIMARY KEY NOT NULL, server_name VARCHAR(100));
+CREATE TABLE IF NOT EXISTS channel(channel_id INT PRIMARY KEY NOT NULL, channel_name VARCHAR(100));
+CREATE TABLE IF NOT EXISTS message(message_id INT PRIMARY KEY NOT NULL, message_writer_id INT NOT NULL, message_content TEXT);
